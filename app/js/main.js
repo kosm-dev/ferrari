a3a92b8c28eaf9fc980f57b9035d35654e7f5b1c
kosm-dev <72253360+kosm-dev@users.noreply.github.com>
2023-12-16 23:46:11 +0200

    added section hero

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,61 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+
+
+
+  if (document.querySelector('.hero-slider')) {
+    const heroSlider = new Swiper('.hero-slider', {
+      loop: true,
+      navigation: {
+        nextEl: '.hero-slider__arrow-next',
+        prevEl: '.hero-slider__arrow-prev',
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+
+    });
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
